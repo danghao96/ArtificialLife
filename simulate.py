c5ee022415350b5bcabb8be03ed61c72f258a288
党皓 <danghao@dhcp-10-105-2-123.wireless.northwestern.private>
2024-01-16 10:48:05 -0600

    Moving car Work Done

--- simulate.py
+++ simulate.py
@@ -1,22 +1,26 @@
-import dm_control.mujoco 
+from dm_control import mujoco
 import mujoco.viewer
 import time
 
-m = dm_control.mujoco.MjModel.from_xml_path("example.xml")
-d = dm_control.mujoco.MjData(m)
-viewer_handle = mujoco.viewer.launch_passive(m, d)
-body1_jntadr = m.body_jntadr[1]  # Assuming body1 is the second body in the XML
-body1_qveladr = body1_jntadr + 3  # For free joints, the first 3 elements are position, next 3 are velocity
+m = mujoco.MjModel.from_xml_path("example.xml")
+d = mujoco.MjData(m)
+m.opt.gravity = (0, 0, -10)
 
-for i in range(2000):
+viewer_handle = mujoco.viewer.launch_passive(m, d)
+for i in range(4000):
     # Set camera parameters
     # These parameters can be adjusted to change the camera angle and perspective
     viewer_handle.cam.azimuth = 0  # Azimuthal angle (in degrees)
     viewer_handle.cam.elevation = -20  # Elevation angle (in degrees)
-    viewer_handle.cam.distance = 15.0  # Distance from the camera to the target
+    viewer_handle.cam.distance = 100.0  # Distance from the camera to the target
     viewer_handle.cam.lookat[:] = [0.0, 0.0, 0.0]  # X-coordinate of the target position
-    d.qvel[body1_qveladr] = 1.0
+
+    car_id = m.geom('car_geom')
+    d.qvel[0] = 10
+
+    print('Generalized positions:', d.qpos)
+    print('Generalized velocities:', d.qvel)
     viewer_handle.sync()
-    dm_control.mujoco.mj_step(m, d)
+    mujoco.mj_step(m, d)
     time.sleep(0.01)
     
